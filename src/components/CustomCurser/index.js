55eb9e7cfd4266e052cff3f93401a7f3cf7736ff
Mohit K. Yadav <mohitkyadav@outlook.com>
2021-05-02 09:10:54 +0530

    added border

--- src/components/CustomCurser/index.js
+++ src/components/CustomCurser/index.js
@@ -4,6 +4,8 @@ import './index.scss'
 
 const CustomCurser = (props) => {
   const [pos, setPos] = useState({ x: 0, y: 0 })
+  // eslint-disable-next-line
+  const [size, setSize] = useState({ width: 30, height: 30 })
 
   useEffect(() => {
     document.addEventListener('mousemove', onMouseOver)
@@ -19,8 +21,10 @@ const CustomCurser = (props) => {
     <div
       className="curser"
       style={{
-        top: pos.y - 15,
-        left: pos.x - 15,
+        top: pos.y - (size.width / 2),
+        left: pos.x - (size.height / 2),
+        width: size.width,
+        height: size.height,
       }}
     />
   )
