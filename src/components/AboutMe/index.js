e85eac45b25746ecb571d847760d7b2ffdb78bba
Mohit K. Yadav <mohitkyadav@outlook.com>
2021-06-13 19:23:47 +0530

    add animation to text

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 import './index.scss'
 
 const msgs = [
   { id: 0, txt: 'I am Mohit Yadav. I love programming and designing.' },
-  { id: 1, txt: 'I am Mohit Yadav. I love programming and designing.' },
-  { id: 2, txt: 'I am Mohit Yadav. I love programming and designing.' },
-  { id: 3, txt: 'I am Mohit Yadav. I love programming and designing.' },
+  { id: 1, txt: 'I like creating illustrations ans designs and I like FPS PC games.' },
+  { id: 2, txt: 'I am also into Anime and Manga.' },
+  { id: 3, txt: 'My element is Water. Life starts from water.' },
 ]
 
-const AboutMe = () => (
-  <div className="about-me">
-    {msgs[0].txt}
-  </div>
-)
+const AboutMe = () => {
+  const [currentText, setCurrentText] = useState(0)
 
+  useEffect(() => {
+    const newCurrentText = currentText === msgs.length - 1 ? 0 : currentText + 1
+
+    const timer = setTimeout(() => setCurrentText(newCurrentText), 2000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  },
+  [currentText])
+
+  return (
+    <div className="about-me">
+
+      <TransitionGroup>
+        <CSSTransition
+          key={msgs[currentText].id}
+          appear
+          timeout={2000}
+          classNames="slide-up"
+        >
+          <p>
+            {msgs[currentText].txt}
+          </p>
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
+  )
+}
 export default AboutMe
