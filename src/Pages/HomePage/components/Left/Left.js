1bd3f45394d5bd2ce45f69be9f500d37ae93bb02
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2022-06-29 21:55:43 +0200

    Create left section

--- src/Pages/HomePage/components/Left/Left.js
+++ src/Pages/HomePage/components/Left/Left.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
+import HomeHero from '../../../../components/HomeHero'
+
 import './Left.scss'
 
 export const Left = () => (
   <div className="home-left">
-    left
+    <div className="home-left__hero">
+      <HomeHero />
+    </div>
+    <div className="home-left__name">
+      <span>Mohit K. Yadav</span>
+      <p>— code & a little design</p>
+    </div>
+    <div className="home-left__description">
+      Software engineer focused on
+      modular design and development.
+    </div>
   </div>
 )
 
