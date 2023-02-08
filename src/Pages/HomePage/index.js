b5602c0052f4ccf07031d75245d29999d8e6346e
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2023-02-08 10:52:01 +0530

    Add lanyard

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useLanyard } from 'react-use-lanyard'
 
 // import BottomNav from '../../components/BottomNav'
 // import HomeHero from '../../components/HomeHero'
@@ -8,11 +9,22 @@ import { Left, Right } from './components'
 
 import './index.scss'
 
-const HomePage = () => (
-  <div className="home-page">
-    <Left />
-    <Right />
-  </div>
-)
+const HomePage = () => {
+  const lanyard = useLanyard({
+    userId: '391828293495816192',
+  })
+
+  if (!lanyard.isValidating) {
+    // eslint-disable-next-line no-console
+    console.log(lanyard.data)
+  }
+
+  return (
+    <div className="home-page">
+      <Left />
+      <Right />
+    </div>
+  )
+}
 
 export default HomePage
