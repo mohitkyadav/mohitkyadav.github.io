6ae238f2a9fe42fe9f8cbe738357aa9f46109a2e
Mohit Kumar Yadav <mohit.yadav@cobalt.io>
2024-04-11 15:10:35 +0200

    remove text

--- src/Pages/HomePage/components/Left/Left.js
+++ src/Pages/HomePage/components/Left/Left.js
@@ -6,19 +6,18 @@ import './Left.scss'
 
 export const Left = () => (
   <div className="home-left animation-slide-down">
+    <div className="home-left__name">
+      <span>Mohit K. Yadav</span>
+    </div>
     <div className="home-left__hero">
       <div className="home-left__hero__wrapper">
         <HomeHero />
       </div>
     </div>
-    <div className="home-left__name">
-      <span>Mohit K. Yadav</span>
-      <p>— code & a little design</p>
-    </div>
-    <div className="home-left__description">
+    {/* <div className="home-left__description">
       इन्द्रियाणि पराण्याहुरिन्द्रियेभ्यः परं मनः।
       <p>Indriyāṇi parāṇyāhurindriyebhyaḥ paraṃ manaḥ।</p>
-    </div>
+    </div> */}
   </div>
 )
 
